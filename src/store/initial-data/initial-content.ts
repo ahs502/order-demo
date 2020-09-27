77a980f57d60a8884d61602395fd8a0ab909afb9
Hessamodding A Shokravi <ahs502@gmail.com>
2020-09-27 17:56:38 +0330

    feat: improve initial data

--- src/store/initial-data/initial-content.ts
+++ src/store/initial-data/initial-content.ts
@@ -21,10 +21,6 @@ const users: readonly User[] = [
     id: "user-id-shahriyar",
     name: "Shahriar Entezam",
   },
-  {
-    id: "user-id-ahmad",
-    name: "Ahmad Bagheri",
-  },
 ];
 
 const firstPage: Page = {
@@ -46,8 +42,62 @@ const firstPage: Page = {
           name: "My image",
           url: "https://google.com",
         },
+        {
+          type: "audio",
+          name: "My audio",
+          url: "https://google.com",
+        },
+        {
+          type: "video",
+          name: "My video",
+          url: "https://google.com",
+        },
       ],
       text: "My second note",
+    },
+    {
+      type: CardType.MARKDOWN,
+      attachments: [
+        {
+          type: "file",
+          name: "My file",
+          url: "https://google.com",
+        },
+      ],
+      markdown: "This is _some_ **kind** of _**markdown**_!",
+    },
+    {
+      type: CardType.TEXT,
+      attachments: [],
+      text: "Another simple text card",
+    },
+    {
+      type: CardType.TODO,
+      attachments: [],
+      label: "To-do cards have a checkbox!",
+      checked: true,
+    },
+    {
+      type: CardType.TODO,
+      attachments: [
+        {
+          type: "link preview",
+          name: "My link preview",
+          url: "https://google.com",
+        },
+        {
+          type: "image",
+          name: "My other image",
+          url: "https://google.com",
+        },
+      ],
+      label: "My other to-do card",
+      checked: false,
+    },
+    {
+      type: CardType.MARKDOWN,
+      attachments: [],
+      markdown: "Another <em>simple</em> markdown",
     },
   ],
 };
@@ -55,8 +105,8 @@ const firstPage: Page = {
 const secondPage: Page = {
   id: "page-id-second",
   name: "My Second Page",
-  owner: users[2],
-  members: [initialCurrentUser, users[0], users[3], users[4]],
+  owner: users[3],
+  members: [initialCurrentUser, users[0], users[2]],
   cards: [
     {
       type: CardType.TEXT,
